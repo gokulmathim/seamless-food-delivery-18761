5a5f2e34687bdd8e334abfb98bf124429e1ed1ce
Kavia AI <production@kavia.ai>
2025-09-08 09:30:10 +0000

    CheckPoint - cg6943fa32

---
+++ .init/.linter.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /home/kavia/workspace/code-generation/seamless-food-delivery-18761/food_delivery_frontend
+flutter analyze
+
---
+++ food_delivery_frontend/android/app/src/main/kotlin/com/example/food_delivery_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.food_delivery_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
